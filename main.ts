4ffd9e7b9ddb978d7c24c7817fc26313a4ce2b01
leontooogod23 <124359921+leontooogod23@users.noreply.github.com>
2023-02-14 16:23:39 +0100

    Initiala filer för MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,33 @@
+basic.showString("Olycka")
+basic.clearScreen()
+basic.pause(100)
+basic.showLeds(`
+    . . # . .
+    # # # # #
+    . . # . .
+    . # . # .
+    # . . . #
+    `)
+basic.clearScreen()
+basic.pause(200)
+basic.showLeds(`
+    . . # . .
+    # # # # #
+    . . # . .
+    . # . # .
+    # . . . #
+    `)
+basic.clearScreen()
+basic.pause(100)
+basic.showLeds(`
+    . . . . #
+    # . . # .
+    # . # . .
+    # # . . .
+    # # # # .
+    `)
+basic.clearScreen()
+basic.pause(1000)
+basic.showNumber(50)
+basic.clearScreen()
+basic.pause(500)
